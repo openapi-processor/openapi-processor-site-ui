af76c15b3155304db59ff8c20ee40d0c0194a0a1
Martin Hauner <martin.hauner@gmx.net>
2020-06-27 23:02:30 +0200

    add tailwind css

--- gulp.d/tasks/build.js
+++ gulp.d/tasks/build.js
@@ -48,6 +48,7 @@ module.exports = (src, dest, preview) => () => {
     ]),
     postcssVar({ preserve: preview }),
     preview ? postcssCalc : () => {},
+    require('tailwindcss'),
     autoprefixer,
     preview
       ? () => {}
