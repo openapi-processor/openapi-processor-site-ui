14af8b074a05181e30615b4553c19f330b3b99a3
Martin Hauner <martin.hauner@gmx.net>
2020-06-28 16:14:30 +0200

    header, toc

--- src/helpers/version.js
+++ src/helpers/version.js
@@ -2,16 +2,16 @@
 
 /**
  * returns the first (newest) version of the given component unless it it the page component. In that
- * case it returns page version.
+ * case it returns the page version.
  *
  * @param component the component
  * @param ctx the antora context (automatically added)
- * @returns the display version string
+ * @returns a version
  */
 module.exports = (component, ctx) => {
   if (component.name === ctx.data.root.page.component.name) {
-    return ctx.data.root.page.displayVersion
+    return ctx.data.root.page.versions.find((v) => v.version === ctx.data.root.page.version)
   } else {
-    return component.versions[0].displayVersion
+    return component.versions[0]
   }
 }
