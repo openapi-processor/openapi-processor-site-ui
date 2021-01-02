c9b48ac17ae7108822cc0b704168a24caf0dcc1f
Martin Hauner <martin.hauner@gmx.net>
2021-01-02 17:32:47 +0100

    keep it

--- versions/tailwind.config.js
+++ versions/tailwind.config.js
@@ -5,13 +5,17 @@ module.exports = {
       './src/**/*.tsx',
       './src/**/*.js',
       './src/**/*.css'
-    ]
+    ],
+    options: {
+      safelist: ['last:mr-0'],
+    },
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
+    margin: ['last'],
     extend: {},
   },
   plugins: [],
